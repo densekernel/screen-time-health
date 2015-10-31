6bd45d5bfcc1d6bf709623e50d33f37dc31f1f44
jonnymanf <me@jonnymanfield.com>
2015-10-31 17:49:17 +0000

    update navbar and sign up forms

--- screenTimeHealth/config/routes.rb
+++ screenTimeHealth/config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
