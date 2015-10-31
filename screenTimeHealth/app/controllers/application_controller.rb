630f9f2c1d3055ef99dceeb56d81ee3f19c66f3a
ralucacocioban <raluca.cocioban@gmail.com>
2015-10-31 17:08:33 +0000

    first push

---
+++ screenTimeHealth/app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :surname)}
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :surname) }
+    # devise_parameter_sanitizer.for(avatar_upload_complete_url) { |u| u.permit(:email, :password, :password_confirmation, :is_over_legal_age, :firstname, :lastname, :telephone, :current_password, :avatar, :avatar_cache) }
+  end
+
+end
---
+++ screenTimeHealth/app/models/family.rb
@@ -0,0 +1,10 @@
+class Family < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true
+  validates :surname, presence: true
+  validates :password, presence: true
+end
