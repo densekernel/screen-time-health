420bc1f4c1aa904f7cbe159f323dc6e26918d6d9
tudor <maxx_77@yahoo.co.uk>
2015-10-31 18:12:43 +0000

    just session reference kid

--- screenTimeHealth/db/migrate/20151031174203_create_sessions.rb
+++ screenTimeHealth/db/migrate/20151031174203_create_sessions.rb
@@ -3,6 +3,7 @@ class CreateSessions < ActiveRecord::Migration
     create_table :sessions do |t|
       t.datetime :startTime
       t.datetime :endTime
+      t.references :kid, index: true
 
       t.timestamps null: false
     end
