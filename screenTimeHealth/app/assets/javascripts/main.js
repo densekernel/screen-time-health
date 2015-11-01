3622634fa5cbdd7a4fd5d5a8ea4a981a59a315f2
ralucacocioban <raluca.cocioban@gmail.com>
2015-11-01 00:41:00 +0000

    charts

--- screenTimeHealth/app/assets/javascripts/main.js
+++ screenTimeHealth/app/assets/javascripts/main.js
@@ -22,6 +22,10 @@ var loadFunc = function() {
         });
       })
     }
+    
+
+           
+
   
 };
 
--- screenTimeHealth/app/controllers/kid_controller.rb
+++ screenTimeHealth/app/controllers/kid_controller.rb
@@ -1,6 +1,31 @@
 class KidController < ApplicationController
   def view
     @kid = Kid.find(params[:id])
-    gon.session = @kid.session
+    
+    @session_data = {}
+
+    current_date = @kid.session.first.startTime.strftime("%d%m%Y")
+    key = current_date + " " + @kid.session.first.startTime.strftime("%A")
+    session_arr = []
+
+    @kid.session.each do |session|
+   
+
+      if(session.startTime.strftime("%d%m%Y") != current_date)
+        @session_data[key] = session_arr
+
+        current_date = session.startTime.strftime("%d%m%Y")
+        key = current_date + " " + session.startTime.strftime("%A")
+      else
+        startTime = session.startTime
+        endTime = session.endTime
+        session_duration_mins = (endTime - startTime) / 60
+        session_arr.push(session_duration_mins)
+      end
+    end
+
+    puts @session_data
+    gon.session = @session_data
+
   end
 end
