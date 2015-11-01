b412e7e6758b95fcfe495eb713741c9ec8d8fc94
tudor <maxx_77@yahoo.co.uk>
2015-11-01 03:47:00 +0000

    obesity charts in family

--- screenTimeHealth/app/controllers/family_controller.rb
+++ screenTimeHealth/app/controllers/family_controller.rb
@@ -4,6 +4,42 @@ class FamilyController < ApplicationController
     @family = current_family
     @kids = current_family.kids
 
+    require 'json'
+	file = File.read('app/assets/jsons/obese_percentages.json')
+	@data_hash = JSON.parse(file)
+
+	@chart = []
+	for each_data_hash in @data_hash
+		data_hash_name = each_data_hash[0]
+		data_hash_data = each_data_hash[1]
+		newchart = LazyHighCharts::HighChart.new('pie') do |f|
+      	f.chart({:defaultSeriesType=>"pie" , :margin=> [0, 0, 0, 0]} )
+      	series = {
+               :type=> 'pie',
+               :name=> '%people',
+               :data=> [
+                  [data_hash_data.keys[0],   data_hash_data.values[0]],
+                  [data_hash_data.keys[1],   data_hash_data.values[1]],
+                  [data_hash_data.keys[2],   data_hash_data.values[2]]
+               ]
+      	}
+      	f.series(series)
+      	f.options[:title][:text] = data_hash_name
+      	f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '5px',:top=> '10px'}) 
+      	f.plot_options(:pie=>{
+        	:allowPointSelect=>true, 
+        	:cursor=>"pointer" , 
+        	:dataLabels=>{
+          		:enabled=>true,
+          		:color=>"black",
+          		:style=>{
+            		:font=>"13px Trebuchet MS, Verdana, sans-serif"
+          			}
+        		}
+      		})
+    	end
+    	@chart.append(newchart)
+  	end
   end
 
   def edit
